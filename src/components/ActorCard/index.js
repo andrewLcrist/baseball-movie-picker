b6391a3b95175aa215c9494f0b6cd549ff14fd2c
Andrew Crist <andrew.l.crist@gmail.com>
2019-03-09 22:18:06 -0600

    add actors and movie trailers

--- src/components/ActorCard/index.js
+++ src/components/ActorCard/index.js
@@ -4,7 +4,9 @@ const ActorCard = ({actor}) => {
 
   return(
       <div className="actor-card-content">
-        <img src={`https://image.tmdb.org/t/p/w200/` + actor.profile_path} />
+        <div className="actor-card-profile-container">
+          <img className="actor-card-profile" src={`https://image.tmdb.org/t/p/w200/` + actor.profile_path} />
+        </div>
       </div>
     )
     }
--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import { filter } from 'lodash';
 import ActorCard from '../ActorCard'
+import YouTube from 'react-youtube';
 
 const MovieModal = ({closeModal, movie}) => {
 
   const director = filter(movie.cast.crew, { job: 'Director' }).map(e => e.name).join(', ');
+  const opts = {
+    playerVars: {
+      autoplay: 0,
+      loop: 1,
+      controls: 1,
+      rel: 0,
+      playsinline: 0,
+      modestbranding: 1,
+      iv_load_policy: 3,
+      width: '100%'
+    }
+  }
 
   return(
       <div className="movie-modal">
@@ -16,6 +29,11 @@ const MovieModal = ({closeModal, movie}) => {
           <div className="actor-card-container">
           {Object.values(movie.cast.cast).map(actor => <ActorCard actor={actor}/>)}
           </div>
+          <YouTube
+            id="montage"
+            videoId={movie.trailer}
+            opts={opts}
+          />
           <img src={`https://image.tmdb.org/t/p/w500/` + movie.backdrop_path} />
         </div>
         <div className="modal-backdrop" onClick={() => closeModal()}></div>
