ff83785f8f032474ec719d95e01eaab7ae3f85c1
Andrew Crist <andrew.l.crist@gmail.com>
2019-03-12 19:26:13 -0500

    Updates

---
+++ precache-manifest.a5da721eda0ef68cfedd924222098474.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "78237a408ac337d2b70d",
+    "url": "/baseball-movie-picker/static/js/runtime~main.046559ed.js"
+  },
+  {
+    "revision": "d720ac42e868e8ced862",
+    "url": "/baseball-movie-picker/static/js/main.800e5e75.chunk.js"
+  },
+  {
+    "revision": "b95ce416fb8895b7a413",
+    "url": "/baseball-movie-picker/static/js/2.a376640a.chunk.js"
+  },
+  {
+    "revision": "d720ac42e868e8ced862",
+    "url": "/baseball-movie-picker/static/css/main.1f60515a.chunk.css"
+  },
+  {
+    "revision": "34d8f718a380b9291a1f437bdd8e97aa",
+    "url": "/baseball-movie-picker/index.html"
+  }
+];
